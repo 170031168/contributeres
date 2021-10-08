c4b5795717eadeca628011901d4db340f5cf3da5
sibashish99 <sibashishbiswas349@gmail.com>
2021-10-08 10:00:50 +0530

    REQ - 110 - NEW BRANCH SIBASHISH 2.0

---
+++ book-service-2/src/main/java/com/lumen/demo/repo/NewRepo.java
@@ -0,0 +1,10 @@
+package com.lumen.demo.repo;
+
+public class NewRepo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Hello from sibashish");
+	}
+
+}
